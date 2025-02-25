adefe955bc3b6470e05bdc4b85e1873a7c5fd3ff
kaansevim <kaansevim111@gmail.com>
2025-02-25 15:21:35 +0300

    Veri tipleri oluşturuldu

---
+++ Veri tipleri.R
@@ -0,0 +1,49 @@
+
+
+Sys.setlocale("LC_CTYPE", "en_us.UTF-8")
+
+### Veri Tipleri
+
+
+# logical variables
+
+a <- TRUE
+b <- FALSE
+
+class(a)
+
+
+# Numeric variables
+
+c <- 3
+class(c)
+
+# Integer variables (tam sayı)
+
+class(6)
+class(6L)
+
+# Complex variables
+
+z <- 3+5i
+class(z)
+
+# Character variables
+
+k <- "merhaba dünya"
+class(k)
+
+h <- "1"
+class(h)
+
+
+
+
+
+
+
+
+
+
+
+
